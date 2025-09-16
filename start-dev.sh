ea494198e5488583e73c4caf42d0eba1c282ed8d
Thomas Bouillon <thomas.bouillon@dataiku.com>
2025-09-16 22:39:40 +0200

    better start script

--- start-dev.sh
+++ start-dev.sh
@@ -13,11 +13,13 @@ SESSION_NAME="dev_session"
 tmux new-session -d -s $SESSION_NAME
 
 # Pane 0: frontend
-tmux send-keys -t $SESSION_NAME:0.0 "cd frontend &&  npm run dev -- --port 8080 --host $HOST" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "cd frontend" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "npm run dev -- --port 8080" C-m
 
 # Split and start api in pane 1
 tmux split-window -v -t $SESSION_NAME
-tmux send-keys -t $SESSION_NAME:0.1 "cd backend/core && uv run python manage.py runserver $HOST:8000" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "cd backend/core" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "uv run python manage.py runserver localhost:8000" C-m
 
 # Optional: Bind 'x' (after Ctrl+b) to kill the session
 tmux bind-key -n C-x kill-session
